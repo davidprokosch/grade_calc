dedc2479a3bd58cb0da77c6bcd41c904bd52cfc7
David <david.pro131996@gmail.com>
2021-08-30 15:30:26 +0200

    getKoncept

--- gradeCalc.R
+++ gradeCalc.R
@@ -1,27 +1,50 @@
 library(R6)
 library(checkmate)
+library(data.table)
 
 Modul <- R6Class("Modul",
                  public = list(
                    # is the Subject a compulsory modul?
-                   compModul <- NULL,
+                   compulsoryModul = NULL,
+                   # Is the Modul composed
+                   composedModul = NULL,
                    # "Kurzbezeichnung des Moduls"
-                   shortNameModul <- NULL,
-                   nameModul <- NULL,
+                   shortNameModul = NULL,
+                   nameModul = NULL,
                    # parts/subjects of the Modul
-                   subjects <- NULL,
-                   ects <- NULL,
-                   createModul <- 
-                     function(compModul, shortNameModul, nameModul, subjects, ects) {
-                       self$compModul <- assertLogical(compModul)
+                   # "Bezeichnung dere Lehrveranstallting" if Module != subject
+                   subjects = NULL,
+                   ects = NULL,
+                   grade = NA,
+                   initialize =
+                     function(compulsoryModul, composedModul, shortNameModul, nameModul, subjects, ects) {
+                       self$compulsoryModul <- assertFlag(compulsoryModul)
+                       self$composedModul <- assertFlag(composedModul)
                        self$shortNameModul <- assertString(shortNameModul)
                        self$nameModul <- assertString(nameModul)
                        self$subjects <- assertCharacter(subjects)
-                       self$ects <- assertNumber(ects)
+                       self$ects <- assertNumeric(ects, lower = 3)
+                     },
+                   getGrade = function(x) {
+                     if (!missing(x)) {
+                       self$grade <- assertNumeric(x, lower = 1.0, upper = 5.0)
                      }
+                     self$grade
+                   }
                  )
 )
 
-list(
-  P1 <- list()
-)
+
+schnittGrade <- function(moduls) {
+  assertList(moduls, types = "Modul")
+  modulsDone <- rbindlist(list(list(modul = NULL, ects = NULL, grade = NULL)))
+  for (i in seq_along(moduls)) {
+    if (!is.na(moduls[[i]]$grade) && moduls[[i]]$grade != 5) {
+      modulsDone <- rbindlist(list(modulsDone,
+                                   list(moduls[[i]]$shortNameModul,
+                                        sum(moduls[[i]]$ects),
+                                        weighted.mean(moduls[[i]]$grade, moduls[[i]]$ects))))
+    }
+  }
+  weighted.mean(modulsDone[, 3], modulsDone[, 2])
+}
