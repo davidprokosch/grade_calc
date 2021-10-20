9e73660ee19d4d688191990dd400736b1e746cce
davidprokosch <66292172+davidprokosch@users.noreply.github.com>
2021-10-20 12:24:53 +0200

    r6 is not anymore

--- Subjects.R
+++ Subjects.R
@@ -32,3 +32,76 @@ subjects <- list(
        "Ausgewaehlte Gebiete der angewandten Statistik A", 6)
 )
 moduls <- lapply(subjects, function(x)  Modul$new(x[[1]], x[[2]], x[[3]], x[[4]], x[[5]], x[[6]]))
+
+                 
+                 calc_subject_grades <- function(list_subjects) {
+  lapply(list_subjects, assert_r6, "Subject")
+  assert_list(list_subjects)
+  # logical vector indicating, if a subject is absolved
+  is_absolved <- vapply(list_subjects, function(x) !is.na(x$grade), logical(1))
+  # asserts if there are any grades to calculate with
+  assert_true(any(is_absolved))
+  subjects_absolved <- list_subjects[is_absolved]
+  moduls_absolved <- lapply(subjects_absolved, function(x) {
+    if (x$composedModul) {
+      
+    }
+  })
+}
+
+
+calculator <- function(list_subjects) {
+  lapply(list_subjects, assert_r6, subject)
+  assert_list(list_subjects)
+  # logical vector indicating, if a subject is absolved
+  subj_absolved <- vapply(list_subjects, function(x) !is.na(x$grade), logical(1))
+  # asserts if there are any grades to calculate with
+  assert_true(any(subj_absolved))
+  subjects_absolved <- list_subjects[subj_absolved]
+}
+
+calc_moduls_grade <- function(subj_dt) {
+  
+}
+
+subjects <- list(
+  list(TRUE, FALSE, "P1", "Einfuehrung in die deskriptive Statistik", "Einfuehrung in die deskriptive Statistik", 6),
+  list(TRUE, FALSE, "P2", "Analysis fuer Informatiker und Statistiker", "Analysis fuer Informatiker und Statistiker", 9),
+  list(TRUE, FALSE, "P3", "Einfuehrung in die Matrizenrechnung", "Einfuehrung in die Matrizenrechnung", 9),
+  list(TRUE, FALSE, "P4", "Einfuehrung in die Wahrscheinlichkeitsrechnung und in die induktive Statistik",
+       "Einfuehrung in die Wahrscheinlichkeitsrechnung und in die induktive Statistik", 12),
+  list(TRUE, FALSE, "P5", "Einfuehrung in die Analysis", "Einfuehrung in die Analysis", 9),
+  list(TRUE, TRUE, "P6", "Einfuehrung in die praktische Statistik",  "Statistische Software", 3),
+  list(TRUE, TRUE, "P6", "Einfuehrung in die praktische Statistik", "Anfaengerpraktikum", 3),
+  list(TRUE, FALSE, "P7", "Grundlagen der Statistik 1", "Wahrscheinlichkeitstheorie und Inferenz 1", 9),
+  list(TRUE, TRUE, "P8", "Grundlagen der Statistik 2", "Wahrscheinlichkeitstheorie und Inferenz 2", 9),
+  list(TRUE, TRUE, "P8", "Grundlagen der Statistik 2", "Bachelor-Seminar", 6),
+  list(TRUE, FALSE, "P9", "Einfuehrung in Lineare Modelle", "Einfuehrung in Lineare Modelle", 9),
+  list(TRUE, TRUE, "P10", "Fortgeschrittene praktische Statistik", "Programieren mit statistischer Software", 6),
+  list(TRUE, TRUE, "P10", "Fortgeschrittene praktische Statistik", "Statistisches Praktikum", 9),
+  list(TRUE, FALSE, "P11", "Generalisierte Regression", "Generalisierte Regression", 9),
+  list(TRUE, FALSE, "P12", "Einfuehrung in die multivariaten Verfahren", "Einfuehrung in die multivariaten Verfahren", 9),
+  # Strictly speeking its just one subject, but we maintain it otherwise
+  list(TRUE, TRUE, "P13", "Abschlussmodul", "Bachelorarbeit", 12),
+  list(TRUE, TRUE, "P13", "Abschlussmodul", "Disputation", 3),
+  list(FALSE, FALSE, "WP1", "Einfuehrung in die Informatik: Programmierung und Software-Entwicklung",
+       "Einfuehrung in die Informatik: Programmierung und Software-Entwicklung", 6),
+  list(FALSE, FALSE, "WP2", "Einfuehrung in die angewandte Statistik", "Einfuehrung in die angewandte Statistik", 6),
+  list(FALSE, FALSE, "WP3", "Stichprobentheorie", "Stichprobentheorie", 6),
+  list(FALSE, FALSE, "WP4", "Wirtschafts- und Sozialstatistik", "Wirtschafts- und Sozialstatistik", 6),
+  list(FALSE, FALSE, "WP5", "Versuchsplanung", "Versuchsplanung", 6),
+  list(FALSE, TRUE, "WP6", "Praktische Statistik und verteilungsfreie Verfahren",
+       "Ausgewaehlte Gebiete der angewandten Statistik B", 3),
+  list(FALSE, TRUE, "WP6", "Praktische Statistik und verteilungsfreie Verfahren",
+       "Praxisprojekt", 3),
+  list(FALSE, TRUE, "WP6", "Praktische Statistik und verteilungsfreie Verfahren",
+       "Verteilungsfreie Verfahren", 3),
+  list(FALSE, FALSE, "WP7", "Zeitreihen", "Zeitreihen", 6),
+  list(FALSE, FALSE, "WP8", "Ausgewaehlte Gebiete der angewandten Statistik A",
+       "Ausgewaehlte Gebiete der angewandten Statistik A", 6)
+)
+subj_dt <- rbindlist(subjects)[, grade := NA_integer_]
+colnames(subj_dt) <- c("compulsoryModul", "composedModul", "shortNameModul",
+                       "nameModul", "subjects", "ects", "grade")
+
+                          
